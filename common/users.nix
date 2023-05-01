0419c7c6bd1283b9e4c4970bea183547a520652f
Michael Bianchi <michael@bianchi.dev>
2023-05-01 11:18:52 -0500

    set user password declaratively

--- common/users.nix
+++ common/users.nix
@@ -1,12 +1,17 @@
+{ config, ... }:
 let
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxN58yRjg+8PNAcgCQzM26K9LLf8nH4J1PEqzXLGhQ5"
   ];
 in
 {
+  sops.secrets.bonky-password = {
+    neededForUsers = true;
+  };
+
   users.users.bonky = {
     isNormalUser = true;
-    # TODO(bianchi): secretize password
+    passwordFile = config.sops.secrets.bonky-password.path;
     # shell = pkgs.zsh;
     createHome = true;
     home = "/home/bonky";
