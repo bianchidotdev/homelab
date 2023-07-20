cba8c67fd7ec8484133d0ac0aac1255de0aba4cb
Bianchi <michael@bianchi.dev>
2023-07-19 20:20:08 -0500

    add data volume

--- workloads/livebook.nix
+++ workloads/livebook.nix
@@ -2,11 +2,14 @@
   sops.secrets."livebook.env" = { };
   virtualisation.oci-containers.containers = {
     livebook = {
-      image = "ghcr.io/livebook-dev/livebook";
+      image = "ghcr.io/livebook-dev/livebook:latest-cuda11.8";
       ports = [
         "8082:8080"
         "8081:8081"
       ];
+      volumes = [
+        "/data/livebook:/data"
+      ]
       environmentFiles = [
         config.sops.secrets."livebook.env".path
       ];
